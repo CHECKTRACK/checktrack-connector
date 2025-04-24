943c43241d9683692b2d232bc2226bbf96ffffca
AyushPatel0722 <ayush.patel@team.satat.tech>
2025-04-24 12:09:02 +0530

    Tenant and Task

--- checktrack_connector/checktrack_connector/doctype/breakdown_maintenance/breakdown_maintenance.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, satat tech llp and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class BreakdownMaintenance(Document):
-	pass
