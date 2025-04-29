f5d255567788ada28fe9239fb6746ec859d4dc05
AyushPatel0722 <ayush.patel@team.satat.tech>
2025-04-29 16:18:22 +0530

    document_type

--- checktrack_connector/checktrack_connector/doctype/employee/employee.py
+++ checktrack_connector/checktrack_connector/doctype/employee/employee.py
@@ -3,10 +3,6 @@
 
 # import frappe
 from frappe.utils.nestedset import NestedSet
-from frappe.model.document import Document
 
 class Employee(NestedSet):
-    def validate(self):
-        first_name = self.first_name or ''
-        last_name = self.last_name or ''
-        self.employee_name = f"{first_name} {last_name}".strip()
+    pass
