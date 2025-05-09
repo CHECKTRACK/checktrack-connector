8c90d35793d66553662762900d09a082aa074c9d
AyushPatel0722 <ayush.patel@team.satat.tech>
2025-05-09 10:46:07 +0530

    project crate

--- checktrack_connector/sync.py
+++ checktrack_connector/sync.py
@@ -11,6 +11,12 @@ def sync_or_update_task_in_mongo(doc, method):
         update_task_in_mongo(doc, method)
     else:
         sync_task_to_mongo(doc, method)
+        
+def sync_or_update_project_in_mongo(doc, method):
+    if doc.mongo_project_id:
+        update_project_in_mongo(doc, method)
+    else:
+        sync_project_to_mongo(doc, method)
 
 def get_app_admin_bearer_auth():
 
@@ -146,6 +152,105 @@ def update_task_in_mongo(doc, method):
             "Content-Type": "application/json"
         }
         response = requests.patch(url, json=payload, headers=task_headers)
+        response.raise_for_status()
+
+    except Exception as e:
+        frappe.log_error(frappe.get_traceback(), "Mongo Update Failed")
+
+
+def sync_project_to_mongo(doc, method):
+
+    USER_API_URL = frappe.get_hooks().get("user_api_url")
+    DATA_API_URL = frappe.get_hooks().get("data_api_url")
+
+    if isinstance(USER_API_URL, list) and USER_API_URL:
+        USER_API_URL = USER_API_URL[0]
+    if isinstance(DATA_API_URL, list) and DATA_API_URL:
+        DATA_API_URL = DATA_API_URL[0]
+    frappe.log_error("Triggered sync_project_to_mongo", f"PROJECT NAME: {doc.name}")
+    company_doc = frappe.get_doc("Company", doc.company)
+    payload = {
+        "name": doc.project_name,
+        "assignedTo": [],
+        "description": doc.description,
+        "status": doc.status,
+        "frappe": {
+            "_id": doc.name,
+            "_ref": doc.doctype,
+            "_title": doc.project_name
+        },
+        "tenant": {
+            "_id": {
+                "$oid" : company_doc.tenant_id
+            },
+            "_ref": "tenants",
+            "_title": f"{company_doc.prefix}"
+        }
+    }
+
+    try:
+        prefix = company_doc.prefix
+        url = f"{DATA_API_URL}/{prefix}_projects"
+        access_token = get_app_admin_bearer_auth()
+        project_headers = {
+            "Authorization": access_token,
+            "Content-Type": "application/json"
+        }
+        response = requests.post(url, json=payload, headers=project_headers)
+        response.raise_for_status()
+
+        mongo_id = get_last_value(response.headers['Location'])
+        if mongo_id:
+            doc.mongo_project_id = mongo_id
+            doc.save(ignore_permissions=True)
+            frappe.logger().info(f"[SYNC SUCCESS] Project '{doc.name}' synced to MongoDB with ID: {mongo_id}")
+        else:
+            frappe.logger().error(f"[SYNC FAILED] Project '{doc.name}' created in MongoDB but no ID returned.")
+
+    except Exception as e:
+        frappe.logger().error(f"[SYNC ERROR] Project '{doc.name}' failed to sync to MongoDB.")
+        frappe.throw(e)
+
+def update_project_in_mongo(doc, method):
+    
+    USER_API_URL = frappe.get_hooks().get("user_api_url")
+    DATA_API_URL = frappe.get_hooks().get("data_api_url")
+
+    if isinstance(USER_API_URL, list) and USER_API_URL:
+        USER_API_URL = USER_API_URL[0]
+    if isinstance(DATA_API_URL, list) and DATA_API_URL:
+        DATA_API_URL = DATA_API_URL[0]
+
+    company_doc = frappe.get_doc("Company", doc.company)
+
+    payload = {
+        "name": doc.project_name,
+        "assignedTo": [],
+        "description": doc.description,
+        "status": doc.status,
+        "frappe": {
+            "_id": doc.name,
+            "_ref": doc.doctype,
+            "_title": doc.project_name
+        },
+        "tenant": {
+            "_id": {
+                "$oid" : company_doc.tenant_id
+            },
+            "_ref": "tenants",
+            "_title": f"{company_doc.prefix}"
+        }
+    }
+
+    try:
+        prefix = company_doc.prefix
+        url = f"{DATA_API_URL}/{prefix}_projects/{doc.mongo_project_id}"
+        access_token = get_app_admin_bearer_auth()
+        project_headers = {
+            "Authorization": access_token,
+            "Content-Type": "application/json"
+        }
+        response = requests.patch(url, json=payload, headers=project_headers)
         response.raise_for_status()
 
     except Exception as e:
