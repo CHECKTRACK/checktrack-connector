7d259aac808ba4ae4caea5b768dd7be2b1823513
0krushang <krushang.patel@team.satat.tech>
2025-05-19 16:59:38 +0530

    Fix: PM Task & naming

--- checktrack_connector/checktrack_connector/doctype/preventive_maintenance_task/preventive_maintenance_task.py
+++ checktrack_connector/checktrack_connector/doctype/preventive_maintenance_task/preventive_maintenance_task.py
@@ -4,17 +4,17 @@
 import frappe
 from frappe.model.document import Document
 
-class DemoPMTask(Document):
+class PreventiveMaintenanceTask(Document):
     pass
     # def before_save(self):
     #     # Log when before_save is triggered
-    #     frappe.log_error(f"DemoPMTask Updated: {self.name}, Status: {self.status}", "DemoPMTask Log")
+    #     frappe.log_error(f"PreventiveMaintenanceTask Updated: {self.name}, Status: {self.status}", "PreventiveMaintenanceTask Log")
 
-    #     # Fetch all Task records linked to this DemoPMTask
+    #     # Fetch all Task records linked to this PreventiveMaintenanceTask
     #     tasks = frappe.get_all(
     #         "Task",
     #         filters={
-    #             "task_type_doc": self.name,  # The task should be linked to this DemoPMTask
+    #             "task_type_doc": self.name,  # The task should be linked to this PreventiveMaintenanceTask
     #             "type": self.doctype         # Ensure the task's type matches this DocType
     #         },
     #         fields=["name", "status"]
@@ -22,9 +22,9 @@ class DemoPMTask(Document):
 
     #     # If no tasks are found, log it for debugging
     #     if not tasks:
-    #         frappe.log_error(f"No linked tasks found for DemoPMTask: {self.name}", "DemoPMTask Log")
+    #         frappe.log_error(f"No linked tasks found for PreventiveMaintenanceTask: {self.name}", "PreventiveMaintenanceTask Log")
 
     #     # Update status of each Task record
     #     for t in tasks:
-    #         frappe.log_error(f"Updating Task: {t.name} with new status: {self.status}", "DemoPMTask Log")
+    #         frappe.log_error(f"Updating Task: {t.name} with new status: {self.status}", "PreventiveMaintenanceTask Log")
     #         frappe.db.set_value("Task", t.name, "status", self.status)
